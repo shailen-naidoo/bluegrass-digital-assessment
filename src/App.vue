b22ff41edd0ff4cae66f5935a2bc6f1a806d8b0e
Shailen Naidoo <naidooshailen648@gmail.com>
2025-03-25 20:30:08 +0200

    * Setup the skeleton as per the wireframe https://www.figma.com/design/qCXQ5ipAdYvtkQ0DKyhYrT/Frontend-WEB?node-id=203-11277&t=xVsnbcSjQcWoWOmo-4

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,27 @@
 <template>
-  <p>Hello World</p>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      v-bind="{ elevation }"
+    >
+      <!--  -->
+    </v-navigation-drawer>
+
+    <v-app-bar v-bind="{ elevation }" />
+
+    <v-main class="main">
+      <!--  -->
+    </v-main>
+  </v-app>
 </template>
+
+<script lang="ts" setup>
+const elevation = ref(0)
+const drawer = ref(true)
+</script>
+
+<style scoped>
+.main {
+  background-color: #FAFAFA;
+}
+</style>
