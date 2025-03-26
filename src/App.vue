7d96bd677ebd3dcb57b3322ea73144d278cfed25
Shailen Naidoo <naidooshailen648@gmail.com>
2025-03-26 20:38:57 +0200

    App.vue
    * Implemented the navigation buttons as per the Figma https://www.figma.com/design/qCXQ5ipAdYvtkQ0DKyhYrT/Frontend-WEB?node-id=203-11281&t=NGBFGrFF7Ex0tx7C-4

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,67 @@
       v-model="drawer"
       v-bind="{ elevation }"
     >
-      <!--  -->
+      <section
+        role="image-container"
+        style="height: 65px"
+      />
+      <v-btn
+        flat
+        prepend-icon="mdi-home"
+        class="text-none justify-start mx-3"
+        style="width: 90%"
+        size="large"
+        :ripple="false"
+        color="#67ADB914"
+        rounded="lg"
+      >
+        <template #prepend>
+          <v-icon color="#67ADB9" />
+        </template>
+        <span style="color: #578388">Dashboard</span>
+      </v-btn>
+      <v-btn
+        flat
+        prepend-icon="mdi-account-box"
+        class="text-none justify-start my-1 mx-3"
+        style="width: 90%"
+        size="large"
+        :ripple="false"
+        rounded="lg"
+      >
+        <template #prepend>
+          <v-icon color="#67ADB9" />
+        </template>
+        <span style="color: #414141">My Profile</span>
+      </v-btn>
+      <v-btn
+        flat
+        prepend-icon="mdi-medical-bag"
+        class="text-none justify-start my-1 mx-3"
+        style="width: 90%"
+        size="large"
+        :ripple="false"
+        rounded="lg"
+      >
+        <template #prepend>
+          <v-icon color="#67ADB9" />
+        </template>
+        <span style="color: #414141">Manage Practises</span>
+      </v-btn>
+      <v-btn
+        flat
+        prepend-icon="mdi-file-document-outline"
+        class="text-none justify-start my-1 mx-3"
+        style="width: 90%"
+        size="large"
+        :ripple="false"
+        rounded="lg"
+      >
+        <template #prepend>
+          <v-icon color="#67ADB9" />
+        </template>
+        <span style="color: #414141">Logs</span>
+      </v-btn>
     </v-navigation-drawer>
 
     <v-app-bar
