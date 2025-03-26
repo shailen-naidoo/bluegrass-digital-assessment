664b63f6a2ad3632081411b9e45f2ed82951c5c4
Shailen Naidoo <naidooshailen648@gmail.com>
2025-03-26 17:59:23 +0200

    App.vue
    * Added the icons required as per the Figma design to the app bar https://www.figma.com/design/qCXQ5ipAdYvtkQ0DKyhYrT/Frontend-WEB?node-id=203-12235&t=AAc9AWEsLcI1nfoj-4

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,38 @@
       <!--  -->
     </v-navigation-drawer>
 
-    <v-app-bar v-bind="{ elevation }" />
+    <v-app-bar
+      v-bind="{ elevation }"
+    >
+      <v-container>
+        <v-row
+          justify="end"
+          align="center"
+        >
+          <v-badge
+            class="mr-4"
+            content="2"
+            color="#FF4842"
+          >
+            <v-icon
+              icon="mdi-bell"
+              color="#9D9D9D"
+            />
+          </v-badge>
+          <v-avatar class="avatar_user_profile">
+            AS
+          </v-avatar>
+          <h3 class="avatar_user_name mx-2">
+            Adrian Stefan
+          </h3>
+          <v-icon
+            icon="mdi-chevron-down"
+            class="avatar_icon mr-10"
+            size="small"
+          />
+        </v-row>
+      </v-container>
+    </v-app-bar>
 
     <v-main class="main">
       <!--  -->
@@ -21,7 +52,25 @@ const drawer = ref(true)
 </script>
 
 <style scoped>
+* {
+  color: #414141
+}
+
 .main {
   background-color: #FAFAFA;
+}
+
+.avatar_user_profile {
+  background-color: #67ADB914;
+  color: #578388;
+}
+
+.avatar_user_name {
+  font-size: 14px;
+  font-weight: 600;
+}
+
+.avatar_icon {
+  color: #67ADB9;
 }
 </style>
